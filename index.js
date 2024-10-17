4c40e3868dcbb3995eaaaf1d03881ffcd5de678d
Mateor13 <mateo.torres@epn.edu.ec>
2024-10-17 16:38:36 -0500

    1

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') //USAR EXPRESS - COMMON
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Mi amor')
-})
+const app = new express() //INICIALIZAR EXPRESS
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+//Ruta /
+app.get('/',(request, response)=>{response.send("Hola")})
+
+app.get('/franquicias',(request, response)=>{response.send("Información de franquicias")})
+
+app.get('/simple',(request, response)=>{response.send("Hamburguesa simple")})
+
+app.use((req, res)=> {res.send("<h1 style='font-size:100px; text-align:center; color: blue;margin-top: 20%;'>Page not found - 404<br><img style='margin-left: 30px; width: 100px; heigth: 100px' src='https://img.freepik.com/fotos-premium/persona-aspecto-triste-cara-triste-cabeza_1083198-4218.jpg'</h1>")})
+
+//Se define el puerto
+app.listen(3000)
+// Mensaje de exito
+console.log("Server - OK")
